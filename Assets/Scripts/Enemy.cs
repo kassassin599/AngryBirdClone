8bb4212990b136d9161589662bbe75f5f84c27fd
Keshav Sharma <kassassin99@gmail.com>
2018-12-01 22:08:11 +0530

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy : MonoBehaviour {
+
+    public float health = 4f;
+
+    public static int EnemiesAlive = 0;
+
+    private void Start()
+    {
+        EnemiesAlive++;
+    }
+
+    private void OnCollisionEnter2D(Collision2D colInfo)
+    {
+        if (colInfo.relativeVelocity.magnitude>health)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        EnemiesAlive--;
+        if (EnemiesAlive<=0)
+        {
+            Debug.Log("SceneManager.LoadScene(0);");
+        }
+
+        Destroy(gameObject);
+    }
+}
